9b595f658c9d32ba3a3f856c91a069b5a4c5d60d
Matthew Zemetskiy <122832270+zemetskiym@users.noreply.github.com>
2023-01-22 13:40:07 -0800

    Create blank Next.js app

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>GitHub REST API</title>
+        <meta name="description" content="A simple REST API for collecting public GitHub user information." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+
+      </main>
+    </>
+  )
+}
