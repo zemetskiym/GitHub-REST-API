8bb5e1db85076b0e5f460797c4421c8f8c2370f1
Matthew Zemetskiy <122832270+zemetskiym@users.noreply.github.com>
2023-01-25 20:41:11 -0800

    Style text and data and add Mona Sans

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,27 +26,35 @@ export default function Home() {
         <meta name="description" content="A simple REST API for collecting public GitHub user information." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preload" href="Mona-Sans.woff2" as="font" type="font/woff2" crossOrigin="anonymous"></link>
       </Head>
       <main>
         <>
-        <h1>GitHub REST API</h1>
-        <form onSubmit={(event) => handleSubmit(event)}>
+        <h1 id={styles.title}>GitHub REST API</h1>
+        <form id={styles.form} onSubmit={(event) => handleSubmit(event)}>
           <input 
+            id={styles.input}
             type="text" 
             value={userSearch.user}
             onChange={(event) => setUserSearch(prev => ({user: event.target.value, submitted: prev.submitted}))}
             placeholder='Search by user...'
           />
-          <button>Submit</button>
+          <button id={styles.submit}>
+            <b>Submit</b>
+          </button>
         </form>
         {userData && (
-          <div>
+          <>
+          <div id={styles.data}>
             {Object.entries(userData).map(([key, value]) => (
-              <div key={key}>
-                <p>{key}: {value == null ? "null" : value}</p>
+              <div key={key} className={styles.keyValue}>
+                <span className={styles.key}>{key}:</span>
+                <div className={styles.vr}></div>
+                <span className={styles.value}>{value == null ? "null" : value}</span>
               </div>
             ))}
           </div>
+          </>
         )}
         </>
       </main>
