36d64332670079c58ea23d1018e2a8f18ae0a996
Matthew Zemetskiy <zemetskiym@proton.me>
2023-02-04 21:40:18 -0800

    Make Profile its own component with styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import Profile from '@/components/Profile'
 
 export default function Home() {
   type UserSearch = {user: string, submitted: boolean}
@@ -67,27 +68,7 @@ export default function Home() {
 
         {userData && (
           <>
-          {!userData.hasOwnProperty("message") && <div id={styles.profile}>
-            <div className={styles.center}>
-              <Image id={styles.profilePic} alt="Avatar URL" src={`${(userData as any).avatar_url}`} width={300} height={300} />
-            </div>
-            <h3 id={styles.name}>{(userData as any).name}</h3>
-            <h4 id={styles.login}>{(userData as any).login}</h4>
-            {(userData as any).bio && <p id={styles.bio}>{(userData as any).bio}</p>}
-            <p id={styles.followers}>
-              {(userData as any).followers}
-              <span style={{color: "#8b949e"}}> followers </span>
-              &bull; {" "}
-              {(userData as any).following} 
-              <span style={{color: "#8b949e"}}> following </span>
-            </p>
-            {(userData as any).location && <p className={styles.additionalInfo}>{(userData as any).location}</p>}
-            {(userData as any).email && <p className={styles.additionalInfo}>{(userData as any).email}</p>}
-            {(userData as any).blog && 
-              <a target="_blank" href={`${(userData as any).blog}`}><p className={styles.additionalInfo}>{(userData as any).blog}</p></a>
-            }
-            {(userData as any).twitter_username && <p className={styles.additionalInfo}>@{(userData as any).twitter_username}</p>}
-          </div>}
+          {!userData.hasOwnProperty("message") && <Profile {...userData} />}
           </>
         )}
         </div>
