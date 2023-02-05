88b90c3f59eb2d5c55c32788a9466bd16e90eede
Matthew Zemetskiy <zemetskiym@proton.me>
2023-02-04 22:16:57 -0800

    Define a TypeScript interface for Profile props

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,29 +1,42 @@
 import styles from "../styles/components/Profile.module.css"
 import Image from "next/image"
 
-export default function Profile (userData: object): any {
+interface Profile {
+  avatar_url: string,
+  name: string | null,
+  login: string,
+  bio: string | null,
+  followers: number | boolean,
+  following: number | boolean,
+  location: string | null,
+  email: string | null,
+  blog: string | boolean,
+  twitter_username: string | null
+}
+
+export default function Profile (userData: Partial<Profile>): any {
   return (
     <>
       <div id={styles.profile}>
         <div className={styles.center}>
-          <Image id={styles.profilePic} alt="Avatar URL" src={`${(userData as any).avatar_url}`} width={300} height={300} />
+          <Image id={styles.profilePic} alt="Avatar URL" src={`${userData.avatar_url}`} width={300} height={300} />
         </div>
-        <h3 id={styles.name}>{(userData as any).name}</h3>
-        <h4 id={styles.login}>{(userData as any).login}</h4>
-        {(userData as any).bio && <p id={styles.bio}>{(userData as any).bio}</p>}
+        <h3 id={styles.name}>{userData.name}</h3>
+        <h4 id={styles.login}>{userData.login}</h4>
+        {userData.bio && <p id={styles.bio}>{userData.bio}</p>}
         <p id={styles.followers}>
-          {(userData as any).followers}
+          {userData.followers}
           <span style={{color: "#8b949e"}}> followers </span>
           &bull; {" "}
-          {(userData as any).following} 
+          {userData.following} 
           <span style={{color: "#8b949e"}}> following </span>
         </p>
-        {(userData as any).location && <p className={styles.additionalInfo}>{(userData as any).location}</p>}
-        {(userData as any).email && <p className={styles.additionalInfo}>{(userData as any).email}</p>}
-        {(userData as any).blog && 
-          <a target="_blank" href={`${(userData as any).blog}`}><p className={styles.additionalInfo}>{(userData as any).blog}</p></a>
+        {userData.location && <p className={styles.additionalInfo}>{userData.location}</p>}
+        {userData.email && <p className={styles.additionalInfo}>{userData.email}</p>}
+        {userData.blog && 
+          <a target="_blank" href={`${userData.blog}`}><p className={styles.additionalInfo}>{userData.blog}</p></a>
         }
-        {(userData as any).twitter_username && <p className={styles.additionalInfo}>@{(userData as any).twitter_username}</p>}
+        {userData.twitter_username && <p className={styles.additionalInfo}>@{userData.twitter_username}</p>}
       </div>
     </>
   )
