82c3ed5a0bbf32b6701d222050df6c780e06bd0d
Matthew Zemetskiy <zemetskiym@proton.me>
2023-02-14 21:37:36 -0800

    Add Navbar for external links and page routing.

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import styles from "../styles/components/Navbar.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Navbar (): JSX.Element {
+    return (
+        <nav id={styles.nav}>
+            <ul className={styles.navSection}>
+                <li>
+                    <Link href="/">
+                        <Image alt="" src="/github.svg" height={35} width={35} />
+                    </Link>
+                </li>
+            </ul>
+            <ul className={styles.navSection}>
+                <li>
+                    <Link className={styles.navLink} target="_blank" href="https://docs.github.com/rest">
+                        Documentation
+                    </Link>
+                </li>
+                <li>
+                    <Link className={styles.navLink} target="_blank" href="https://github.com/zemetskiym/github-rest-api">
+                        Repository source
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
