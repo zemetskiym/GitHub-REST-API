168207b0dacf55fc942a8c88b39e770707e6e042
Matthew Zemetskiy <zemetskiym@proton.me>
2023-02-18 00:10:31 -0800

    Add OAuth login through Navbar and _app wrapper

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import styles from "../styles/components/Navbar.module.css"
 import Image from "next/image"
 import Link from "next/link"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Navbar (): JSX.Element {
+    const { data: session } = useSession()
+
     return (
         <nav id={styles.nav}>
             <ul className={styles.navSection}>
@@ -23,6 +26,12 @@ export default function Navbar (): JSX.Element {
                         Repository source
                     </Link>
                 </li>
+                {!session && <li onClick={() => signIn()} className={styles.navLink}>
+                    Sign in
+                </li>}
+                {session && <li onClick={() => signOut()} className={styles.navLink}>
+                    Sign out
+                </li>}
             </ul>
         </nav>
     )
