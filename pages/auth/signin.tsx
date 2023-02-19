d2c6bf9f4f2cad88cc0464daf31713858aa8c116
Matthew Zemetskiy <zemetskiym@proton.me>
2023-02-19 00:19:30 -0800

    Remove unused handleSignIn function

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -3,10 +3,6 @@ import Image from 'next/image';
 import styles from "../../styles/components/signin.module.css"
 
 export default function LoginPage() {
-  const handleSignIn = (e: Event) => {
-    e.preventDefault();
-    signIn("github", { callbackUrl: "https://github-rest-user.netlify.app/api/auth/callback/github", scope: "read:user" });
-  }
   return (
     <div id={styles.main}>
         <div id={styles.logos}>
