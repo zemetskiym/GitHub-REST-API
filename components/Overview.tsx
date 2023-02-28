e6313539678c6af2f22363d00d81b550fc29de6c
Matthew Zemetskiy <zemetskiym@proton.me>
2023-02-27 19:20:01 -0800

    Comment Overview for readability

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -1,8 +1,10 @@
+// Importing necessary dependencies
 import styles from "../styles/components/Overview.module.css"
 import { useState, useEffect } from "react"
 import { marked } from 'marked';
 import Link from "next/link";
 
+// Interface describing user object properties
 interface User {
     login: string,
     id: number,
@@ -37,12 +39,17 @@ interface User {
     updated_at: string
 }
 
+// Component definition
 export default function Overview (userData: Partial<User>): JSX.Element {
+    // State variables declaration
     const [readme, setReadme] = useState<any>(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<any>(null);
+
+    // Interface describing Readme object properties
     interface Readme {content: string}
 
+    // Side effect to fetch the user's profile readme
     useEffect(() => {
         async function fetchData () {
             setLoading(true)
@@ -60,14 +67,17 @@ export default function Overview (userData: Partial<User>): JSX.Element {
         fetchData()
     }, [userData])
     
+    // If an error occurs during the fetch, a message with a link to the documentation is displayed
     if (error) return (
         <div id={styles.error}>This user has no profile readme. For more information, visit the documentation 
             <Link id={styles.documentation} target="_blank" href="https://docs.github.com/en/account-and-profile/setting-up-and-managing-your-github-profile/customizing-your-profile/managing-your-profile-readme">here</Link>.
         </div>
     )
 
+    // While the fetch is in progress, a loading message is displayed
     if (loading) return (<div id={styles.loading}>Loading...</div>)
 
+    // If no error occurs and the fetch is successful, the user's profile readme is displayed
     return (
         <div id={styles.readmeContainer}>
             <small id={styles.title}>{userData.login} <span style={{color: "#727a83"}}>/</span> README<span style={{color: "#727a83"}}>.md</span></small>
