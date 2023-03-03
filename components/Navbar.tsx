29b84ad36978672b3468b3e30fc21c2b64033188
Matthew Zemetskiy <zemetskiym@proton.me>
2023-03-02 20:59:26 -0800

    Comment Navbar for readability

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,15 @@
+// Importing necessary dependencies and styles
 import styles from "../styles/components/Navbar.module.css"
 import Image from "next/image"
 import Link from "next/link"
 import { useSession, signIn, signOut } from "next-auth/react"
 
+// Defining the Navbar component
 export default function Navbar (): JSX.Element {
+    // Retrieving the user's session data
     const { data: session } = useSession()
 
+    // Rendering the navbar with links and optional sign in/out button
     return (
         <nav id={styles.nav}>
             <ul className={styles.navSection}>
@@ -26,9 +30,11 @@ export default function Navbar (): JSX.Element {
                         Repository source
                     </Link>
                 </li>
+                {/* Rendering the sign in button if the user is not signed in */}
                 {!session && <li onClick={() => signIn()} className={styles.navLink}>
                     Sign in
                 </li>}
+                {/* Rendering the sign out button if the user is signed in */}
                 {session && <li onClick={() => signOut()} className={styles.navLink}>
                     Sign out
                 </li>}
