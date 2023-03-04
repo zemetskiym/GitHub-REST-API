8cd360e16518bc0473f1688dd906726da2ed9a68
Matthew Zemetskiy <zemetskiym@proton.me>
2023-03-03 18:32:09 -0800

    Comment SectionNav for readability

--- components/SectionNav.tsx
+++ components/SectionNav.tsx
@@ -1,17 +1,22 @@
+// Import CSS styles and Next.js Image component
 import styles from "../styles/components/SectionNav.module.css";
 import Image from "next/image";
 
+// Define Props interface for the component's props
 interface Props {
   section: string
   setSection: React.Dispatch<React.SetStateAction<string>>
 }
 
+// Define SectionNav component with Props interface
 export default function SectionNav({ section, setSection }: Props): JSX.Element {
+    // Define underlineSelection function to add border styling to the selected section
     function underlineSelection (item: string) {
-        if (section == item) return ({borderColor: "#f78166"})
-        else return ({border: "none"})
+        if (section == item) return ({borderColor: "#f78166"}) // If the section is selected, add border color
+        else return ({border: "none"}) // Otherwise, remove border
     }
 
+    // Return JSX for the component
     return (
         <nav id={styles.sectionNav}>
             <ul id={styles.sectionList}>
