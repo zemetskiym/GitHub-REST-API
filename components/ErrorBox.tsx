bd3487f4a8b3dca6d9e8eac15861aa124be209ec
Matthew Zemetskiy <zemetskiym@proton.me>
2023-03-25 21:10:14 -0700

    Feat: add alert SVG to error box for usability

--- components/ErrorBox.tsx
+++ components/ErrorBox.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/components/ErrorBox.module.css"
+import Image from "next/image"
 
 export default function ErrorBox ({error, setError}: {error: string | null, setError: React.Dispatch<React.SetStateAction<string | null>>}): JSX.Element | null {
     // If no error exists, return without an element
@@ -7,7 +8,10 @@ export default function ErrorBox ({error, setError}: {error: string | null, setE
     // If an error exists, return the error box
     return (
         <div id={styles.errorBox} onClick={() => setError(null)}>
-            <small id={styles.title}>Error</small>
+            <div id={styles.topSection}>
+                <Image src="/alert-fill.svg" alt="" height={20} width={20} />
+                <small id={styles.title}>Error</small>
+            </div>
             <small id={styles.message}>{error}</small>
         </div>
     )
