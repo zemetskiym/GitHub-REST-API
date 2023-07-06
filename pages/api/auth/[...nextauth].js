55ae5465a054bd7d2f65b73bae2c799ab2f89e8e
Matthew Zemetskiy <zemetskiym@proton.me>
2023-07-05 18:36:23 -0700

    Refactor: comment NextAuth config

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,13 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth" // Importing NextAuth library
+import GithubProvider from "next-auth/providers/github" // Importing the Github provider from NextAuth
 
 export const authOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    GithubProvider({ // Configuring the Github provider
+      clientId: process.env.GITHUB_ID, // Setting the Github client ID using environment variable
+      clientSecret: process.env.GITHUB_SECRET, // Setting the Github client secret using environment variable
     }),
   ],
 }
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions) // Exporting the NextAuth instance configured with the provided options
