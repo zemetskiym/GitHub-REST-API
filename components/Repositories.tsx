b59507f907e34b9406b8719d5932f74e75beb96e
zemetskiym <zemetskiym@proton.me>
2023-07-11 22:20:37 -0700

    Fix: make updated field readable

--- components/Repositories.tsx
+++ components/Repositories.tsx
@@ -25,6 +25,34 @@ export default function Repositories({ repoData }: any): JSX.Element {
     function capitalize(string: string) {
         return string?.charAt(0).toUpperCase() + string.slice(1)
     }
+	
+	// Function to convert ISO string to how long ago
+	function getRelativeTime(isoString: string) {
+		const date = new Date(isoString);
+		const currentDate = new Date();
+		const timeDiff = currentDate.getTime() - date.getTime();
+		
+		const seconds = Math.floor(timeDiff / 1000);
+		const minutes = Math.floor(seconds / 60);
+		const hours = Math.floor(minutes / 60);
+		const days = Math.floor(hours / 24);
+		const weeks = Math.floor(days / 7);
+		const months = Math.floor(days / 30.44);
+
+		if (months > 0) {
+			return `${months} month${months > 1 ? 's' : ''} ago`;
+		} else if (weeks > 0) {
+			return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
+		} else if (days > 0) {
+			return `${days} day${days > 1 ? 's' : ''} ago`;
+		} else if (hours > 0) {
+			return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+		} else if (minutes > 0) {
+			return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+		} else {
+			return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+		}
+	}
 
     return (
         // Wrapper div for entire component
@@ -75,7 +103,7 @@ export default function Repositories({ repoData }: any): JSX.Element {
                         </>
                         }
                         {/* Repository last updated time */}
-                        <small className={styles.updatedAt}>Updated {repo.updated_at}</small>
+                        <small className={styles.updatedAt}>Updated {getRelativeTime(repo.updated_at)}</small>
                     </div>
                 </div>
             )
